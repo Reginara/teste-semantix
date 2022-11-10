084ed9200059ed70468dff7835fc6aa7d054ce3b
Reginara Gomes <refagomes@gmail.com>
2022-11-10 13:05:33 -0300

    Backend start - trying integrate the database

---
+++ backend/src/models/user.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  confirmPassword: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  userGroup: {
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
