4d388ecec61f0eb48081cbf454db53bc5e240b5a
Reginara Gomes <refagomes@gmail.com>
2022-11-11 22:08:24 -0300

    add page-login structure

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,27 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponentComponent } from './components/header-component/header-component.component';
-import { SideNavComponentComponent } from './components/side-nav-component/side-nav-component.component';
-import { MenuViewComponent } from './pages/menu-view/menu-view.component';
-
-
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
-import { UsergroupListComponent } from './components/usergroup-list/usergroup-list.component';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/menu-list-components/header/header.component';
+import { SidenavComponent } from './components/menu-list-components/sidenav/sidenav.component';
+import { MenuViewComponent } from './pages/menu-view/menu-view.component';
+import { UsergroupListComponent } from './components/menu-list-components/usergroup-list/usergroup-list.component';
+import { LoginViewComponent } from './pages/login-view/login-view.component';
+import { PageLoginComponent } from './components/login-components/page-login/page-login.component';
+import { CardLoginComponent } from './components/login-components/card-login/card-login.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponentComponent,
-    SideNavComponentComponent,
+    HeaderComponent,
+    SidenavComponent,
     MenuViewComponent,
     UsergroupListComponent,
+    LoginViewComponent,
+    PageLoginComponent,
+    CardLoginComponent,
   ],
   imports: [
     BrowserModule,
